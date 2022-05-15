9de8b5c9368a17b656d8e72d8b9008df46c01717
Sumukh <sumukhn06@gmail.com>
2022-05-15 19:10:27 +0530

    Initial commit

---
+++ src/main/java/com/learning/kafkastreamslatest/KafkaStreamsLatestApplication.java
@@ -0,0 +1,13 @@
+package com.learning.kafkastreamslatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamsLatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStreamsLatestApplication.class, args);
+	}
+
+}
